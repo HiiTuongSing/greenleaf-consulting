7525302016477882531a09be67909558f341de03
HiiTuongSing <117565834+HiiTuongSing@users.noreply.github.com>
2024-05-30 21:22:12 +0800

    Added Routes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,41 @@
+<script>
+    import "../app.css";
+    import { base } from "$app/paths";
+</script>
+  
+<header>
+  <nav class='flex w-full lg:w-3/4 m-auto justify-around h-[10vh] items-center bg-black text-white'>
+    <a href="{base}/" class="hover:text-white"><i class="fa-solid fa-leaf"></i> GreenLeaf Consulting</a>
+    <ul class="flex justify-around w-2/4">
+      <li class="hover:text-white">
+        <a href="{base}/services">Services</a>
+      </li>
+      <li class="hover:text-white">
+        <a href="{base}/about">About Us</a>
+      </li>
+      <li class="hover:text-white">
+        <a href="{base}/blog">Blog</a>
+      </li>
+      <li class="hover:text-white">
+        <a href="{base}/contact">Contact Us</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
+<slot />
+
+<footer>
+  <div class="w-3/4 m-auto flex justify-around">
+    <div>
+      <h1>GreenLeaf Consultant</h1>
+    </div>
+    <div class="w-2/4 flex justify-around">
+      <h1>Services</h1>
+  
+      <h1>About Us</h1>
+    </div>
+  </div>
+</footer>
+  
+  
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="h-[50vh] w-3/4 m-auto ">
+    <div>
+        <h1 class="text-6xl font-bold">Hero Image</h1>
+        <p>Some description here.</p>
+    </div>
+</div>
